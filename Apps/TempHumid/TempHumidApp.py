0b0e5d18bac5971ae20f461068d2f98fb61c764a
victorsanborn <victor_sanborn@live.se>
2019-10-21 18:54:48 +0200

    test

--- Apps/TempHumid/TempHumidApp.py
+++ Apps/TempHumid/TempHumidApp.py
@@ -3,10 +3,14 @@ import sys
 import Adafruit_DHT as dht
 import threading
 
+def sendData():
+    print 'test'
+
 def printit():
     threading.Timer(5.0, printit).start()
     humidity, temperature = dht.read_retry(11, 4)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    sendData()
 
 
 printit()
