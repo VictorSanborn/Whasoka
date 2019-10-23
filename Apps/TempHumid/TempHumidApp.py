612fc5cc58091940610e61974cad6d2fadb0c0d2
victorsanborn <victor_sanborn@live.se>
2019-10-23 19:42:56 +0100

    working connection between rpi unit and databroker

--- Apps/TempHumid/TempHumidApp.py
+++ Apps/TempHumid/TempHumidApp.py
@@ -1,40 +1,64 @@
 #!/usr/bin/python
 import os
 import sys
-#import Adafruit_DHT as dht
+import Adafruit_DHT as dht
 import threading
+import time
 import socketio
 from dotenv import load_dotenv
 load_dotenv()
 
 
 sio = socketio.Client()
-
+sendTimer = 5000 #Milisecounds
+myId = "" #Recived from socketIO databroker
 
 @sio.event
 def connect():
     print('connection established')
 
-
-@sio.event
-def my_message(data):
-    print('message received with ')
-    sio.emit(
-            'value', '{"me":"test", "myKey": "","myId": "", target: ""}')
-
-
 @sio.event
 def disconnect():
     print('disconnected from server')
 
+#Recived from databroker when the server SocketIO detects the units ID
+@sio.event
+def id(data):
+    myId = data
+
+#Recived from databroker when a update of time is sent to this client ID (myId)
+@sio.event
+def updateTimer(data):
+    sendTimer = data 
+
+#Base message to be able to speak to databroker. (should be untoutched if you are not sure what is happening)
+@sio.event
+def my_message(data):
+    sio.emit('value', '{"me":"'+os.getenv("MY_NAME")+'", "myKey":"'+str(os.getenv("MY_KEY"))+'", "myId":"'+ myId +'", "target":"'+ os.getenv("TARGET_APP") +'", "value": '+data+'}')
+    print("~sent~")
+
+
+
+
 
 def printit():
-    threading.Timer(5.0, printit).start()
-   # humidity, temperature = dht.read_retry(11, 4)
-    #my_message('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
-    my_message('Hi from rpi!')
+    #
+    #       THIS IS AN EXAMPLE APP, REPLACE DATA WITHIN WHILE LOOP TO WRITE YOUR OWN! :) 
+    #
+    while True:
+        time.sleep(sendTimer/1000)
+        humidity, temperature = dht.read_retry(11, 4)
+        room = "livingroom"
 
+        #Data is the value stored in the db. Each key needs to match teh db column name!
+        data = '{"room": "'+room+'", "temp": "'+str(temperature)+'", "humid": "'+str(humidity)+'"}'
+        my_message(data)
+
+#        
+#Basic init of app
+#
+
+#Connect to databroaker socket io server
+sio.connect(str(os.getenv("SOCKET_IO_ADRESS")))
+#Run app
 printit()
-sio.connect('http://192.168.1.101:4001')
-sio.wait()
-
