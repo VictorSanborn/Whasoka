9c5e643a63fe41d7e730178ea18e6d01718dc4ab
victorsanborn <victor_sanborn@live.se>
2019-10-30 20:38:27 +0000

    Started on docker for databroker

---
+++ databroker/scripts/runDocker.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -p 4001:4001 --env SOCKET_IO_PORT=4001 --env SOCKET_IO_ADRESS=http://192.168.1.181:4001 --env GRAPHQL_ADRESS=http://192.168.1.101:9001 --env MY_KEY=sUpErS4f3_No0t wdb
