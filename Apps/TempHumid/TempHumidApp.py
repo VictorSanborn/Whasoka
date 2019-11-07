253c2e2d069ba6161e3fdb39524834a0aa56a9fd
victorsanborn <victor_sanborn@live.se>
2019-11-07 19:29:57 +0000

    try catch for app

--- Apps/TempHumid/TempHumidApp.py
+++ Apps/TempHumid/TempHumidApp.py
@@ -16,34 +16,49 @@ myId = ""  # Recived from socketIO databroker
 
 @sio.event
 def connect():
-    print('connection established')
-    isCon = True
+    try:
+        print('connection established')
+        isCon = True
+    except:
+        print("An exception in connect")
 
 
 @sio.event
 def disconnect():
-    print('disconnected from server')
-    isCon = False
+    try:
+        print('disconnected from server')
+        isCon = False
+    except:
+        print("An exception in disconnect")
 
 # Recived from databroker when the server SocketIO detects the units ID
 @sio.event
 def id(data):
-    print("ID: "+data)
-    global myId
-    myId = data
+    try:
+        #print("ID: "+data)
+        global myId
+        myId = data
+    except:
+        print("An exception in id")
 
 # Recived from databroker when a update of time is sent to this client ID (myId)
 @sio.event
 def settings(data):
-    print("settings data!")
-    global sendTimer
-    sendTimer =  json.loads(data)["sendInterval"]
+    try:
+        #print("settings data!")
+        global sendTimer
+        sendTimer =  json.loads(data)["sendInterval"]
+    except:
+        print("An exception in settings")
 
 # Base message to be able to speak to databroker. (should be untoutched if you are not sure what is happening)
 @sio.event
 def my_message(data):
-    sio.emit('value', '{"me":"'+os.getenv("MY_NAME")+'", "myKey":"'+str(os.getenv("MY_KEY")) +
-             '", "myId":"' + myId + '", "target":"' + os.getenv("TARGET_APP") + '", "value": '+data+'}')
+    try:
+        sio.emit('value', '{"me":"'+os.getenv("MY_NAME")+'", "myKey":"'+str(os.getenv("MY_KEY")) +
+                '", "myId":"' + myId + '", "target":"' + os.getenv("TARGET_APP") + '", "value": '+data+'}')
+    except:
+        print("An exception in my_message")
 
 
 def getTimer():
@@ -54,15 +69,21 @@ def printit():
     #       THIS IS AN EXAMPLE APP, REPLACE CODE WITHIN WHILE LOOP TO WRITE YOUR OWN! :)
     #
     while True:
-        print(getTimer())
-        time.sleep(sendTimer/1000)
-        humidity, temperature = dht.read_retry(11, 4)
-        room = "livingroom"
+        try:
+            #print(getTimer())
+            time.sleep(sendTimer/1000)
+            humidity, temperature = dht.read_retry(11, 4)
+            room = "livingroom"
+        except:
+            print("An exception in section 1")
         
-        print(str(temperature)+"C " + str(humidity)+"%")
-        #Data is the value stored in the db. Each key needs to match teh db column name!
-        data = '{"room": "'+room+'", "temp": "'+str(temperature)+'", "humid": "'+str(humidity)+'"}'
-        my_message(data)
+        try:
+            #print(str(temperature)+"C " + str(humidity)+"%")
+            #Data is the value stored in the db. Each key needs to match teh db column name!
+            data = '{"room": "'+room+'", "temp": "'+str(temperature)+'", "humid": "'+str(humidity)+'"}'
+            my_message(data)
+        except:
+            print("An exception in ection 2")
 
 
 
--- Apps/TempHumid/scripts/runDocker.sh
+++ Apps/TempHumid/scripts/runDocker.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --privileged --env SOCKET_IO_ADRESS=http://192.168.1.101:4001 --env MY_KEY=sUpErS4f3_No0t --env MY_NAME=BedRoomTemp --env TARGET_APP=TempHumidApp w_tha
+sudo docker run -d --privileged --env SOCKET_IO_ADRESS=http://192.168.1.181:4001 --env MY_KEY=sUpErS4f3_No0t --env MY_NAME=BedRoomTemp --env TARGET_APP=TempHumidApp w_tha
